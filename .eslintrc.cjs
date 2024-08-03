a238443899170c9cd01e4d568cd56e0e18b98432
Denis Matveev <matveevdenis458@gmail.com>
2024-08-03 17:47:37 +0300

    Added the ability to write tests

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,24 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react/jsx-runtime', 'plugin:react-hooks/recommended', 'plugin:storybook/recommended'],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  env: { browser: true, es2020: true, node: "current", "jest/globals": true },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:jest/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "plugin:storybook/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
+  plugins: ["react-refresh", "jest"],
   rules: {
-    'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
+    "react/jsx-no-target-blank": "off",
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    "jest/no-focused-tests": "off",
   },
-}
+};
--- src/components/UI/Button/Button.jsx
+++ src/components/UI/Button/Button.jsx
@@ -3,7 +3,8 @@ import s from "./button.module.scss";
 export default function Button({ children, className, props }) {
   return (
     <button className={`${s.button} ${className}`} {...props}>
-      {children}
+      {/* {children} */}
+      <p>Hello World!</p>
     </button>
   );
 }
